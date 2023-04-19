92c3a8463dd85da6a2d5c2d8abd3f966f5163f4f
hakim-asrori <saneglos005@gmail.com>
2023-04-19 13:35:24 +0700

    hash make password

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\Auth\RegisterRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Spatie\Permission\Models\Role;
 
 class RegisterController extends Controller
@@ -28,6 +29,10 @@ class RegisterController extends Controller
         DB::beginTransaction();
 
         $role = Role::findOrFail($this->user::SISWA);
+
+        $request->merge([
+            "password" => Hash::make($request->password),
+        ]);
 
         try {
             $user = $this->user->create($request->all());
