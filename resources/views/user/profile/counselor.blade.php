08e89abb45c11e93eaae22ee13561bee7ce002a6
hakim-asrori <saneglos005@gmail.com>
2023-06-02 13:45:35 +0700

    handling error in profile

--- resources/views/user/profile/counselor.blade.php
+++ resources/views/user/profile/counselor.blade.php
@@ -11,17 +11,17 @@
             <div class="mb-3">
                 <label>Usia</label>
                 <input type="text" name="age" class="form-control input-rounded shadow-sm"
-                    value="{{ $user->counselor->age }}">
+                    value="{{ $user->counselor?->age }}">
             </div>
             <div class="mb-3">
                 <label>Pendidikan</label>
                 <input type="text" name="study" class="form-control input-rounded shadow-sm"
-                    value="{{ $user->counselor->study }}">
+                    value="{{ $user->counselor?->study }}">
             </div>
             <div class="mb-3">
                 <label>Jurusan</label>
                 <input type="text" name="major" class="form-control input-rounded shadow-sm"
-                    value="{{ $user->counselor->major }}">
+                    value="{{ $user->counselor?->major }}">
             </div>
             <div class="mb-3">
                 <label>Email</label>
--- resources/views/user/profile/teacher.blade.php
+++ resources/views/user/profile/teacher.blade.php
@@ -11,22 +11,22 @@
             <div class="mb-3">
                 <label>Usia</label>
                 <input type="text" name="age" class="form-control input-rounded shadow-sm"
-                    value="{{ $user->teacher->age }}">
+                    value="{{ $user->teacher?->age }}">
             </div>
             <div class="mb-3">
                 <label>Pendidikan</label>
                 <input type="text" name="study" class="form-control input-rounded shadow-sm"
-                    value="{{ $user->teacher->study }}">
+                    value="{{ $user->teacher?->study }}">
             </div>
             <div class="mb-3">
                 <label>Jurusan</label>
                 <input type="text" name="major" class="form-control input-rounded shadow-sm"
-                    value="{{ $user->teacher->major }}">
+                    value="{{ $user->teacher?->major }}">
             </div>
             <div class="mb-3">
                 <label>Minat</label>
                 <input type="text" name="interest" class="form-control input-rounded shadow-sm"
-                    value="{{ $user->teacher->interest }}">
+                    value="{{ $user->teacher?->interest }}">
             </div>
             <div class="mb-3">
                 <label>Email</label>
